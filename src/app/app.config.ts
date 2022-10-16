f235a5fbdc4eca819b9b6aa3d0a8f88d806ae88b
raemilcorniel <raemilcorniel@hotmail.com>
2022-10-16 11:41:50 -0400

    environments

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,8 @@
 
 export default {
   oidc: {
-    clientId: '0oa6swbgjmTw1UyQK5d7',
-    issuer:'https://dev-27108252.okta.com/oauth2/default',
+    clientId: 'your_clientid',
+    issuer:'https://yourdomain/oauth2/default',
     redirectUri:  window.location.origin +  '/login/callback',
     // scopes: ['openid', 'profile', 'email'],
     // testing: {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiURl: 'https://localhost:7256/',
-  oktaBaseURL: 'https://dev-27108252.okta.com/oauth2/default/v1/'
+  oktaBaseURL: 'https://yourdomain/oauth2/default/v1/'
 };
